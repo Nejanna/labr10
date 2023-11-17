417c575f4dde60fc5f28088f2d51842a2d32bb5a
nejanna <nejanna288@gmail.com>
2023-11-17 15:12:15 +0200

    Add project files.

---
+++ Controllers/FormController.cs
@@ -0,0 +1,37 @@
+﻿using labr10.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Diagnostics;
+
+namespace laboratory10.Controllers
+{
+    public class FormController : Controller
+    {
+        [HttpGet]
+        public IActionResult CreateForm() => View();
+
+        [HttpPost]
+        public IActionResult Luck(FliedDataModel model)
+        {
+            if (DateTime.Now.Date >= model.Date.Date || model.Date.DayOfWeek == DayOfWeek.Saturday || model.Date.DayOfWeek == DayOfWeek.Sunday)
+                {
+                    ModelState.AddModelError("Date", "Бажана дата повинна бути в майбутньому та не попадати на вихідні");
+                }
+
+            if (model.Product == "Основи" && model.Date.DayOfWeek == DayOfWeek.Monday)
+            {
+                ModelState.AddModelError("Date", "Консультація щодо продукту «Основи» не може проходити по понеділках");
+            }
+
+            if (!ModelState.IsValid)
+            {
+                return View("CreateForm", model);
+            }
+
+            return View("Luck");
+        }
+        }
+
+
+   
+}
---
+++ Models/FliedDataModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace labr10.Models
+{
+    public class FliedDataModel
+    {
+        //Required Цей атрибут вказує на те, що цим властивостям необхідно обов'язково передати деякі значення.
+        [Required(ErrorMessage = "Поле Ім'я прізвище є обов'язковим")]
+        public string FullName { get; set; }
+
+        [Required(ErrorMessage = "Поле Email є обов'язковим")]
+        [EmailAddress(ErrorMessage = "Введіть коректний Email")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Поле дата консультації є обов'язковим")]
+        [DataType(DataType.Date)]
+        public DateTime Date { get; set; }
+
+        [Required(ErrorMessage = "Поле продукту є обов'язковим")]
+        public string Product { get; set; }
+    }
+}
